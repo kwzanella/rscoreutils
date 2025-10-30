c5ee7828f6239af57c9a69cecd0d4f6a9bb7e9cc
kwzanella <jg.zanella@proton.me>
2025-10-30 16:49:07 -0300

    rscat - Added stdin logic

--- rscat/src/main.rs
+++ rscat/src/main.rs
@@ -11,9 +11,10 @@
  * 8. Integration tests
 */
 
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, stdin};
 use std::fs::File;
 use clap::{Parser, command};
+use std::path::Path;
 
 #[derive(Parser, Debug)]
 #[command(version, long_about = "Concatenate FILE(s) to standard output.")]
@@ -27,15 +28,15 @@ struct Args {
     number_nonblank: bool
 }
 
-fn print_file(f_path: File, line_counter: &mut usize) -> std::io::Result<()> {
-    // Decreases system calls by buffering file
-    let mut reader = BufReader::new(f_path);
+fn print_file<R: BufRead>(reader: &mut R, line_counter: &mut usize) -> std::io::Result<()> {
     let mut line = String::new();
     let mut num_bytes: usize;
+
     loop {
         // Clear buffer before reading next line
         line.clear();
 
+        // This may fail if file is not UTF-8
         num_bytes = reader.read_line(&mut line)?;
 
         // EOF has been found
@@ -49,36 +50,73 @@ fn print_file(f_path: File, line_counter: &mut usize) -> std::io::Result<()> {
     }
 }
 
+fn check_files(file_paths: &[String]) -> bool {
+    for p in file_paths {
+
+        // No need to check if stdin exists
+        if p == "-" {
+            continue;
+        }
+
+        if !Path::new(p).exists() {
+            eprintln!("File {p} does not exist");
+            return false;
+        }
+    }
+    return true;
+}
+
 fn main() -> std::io::Result<()> {
-    let args = Args::parse();
+    let mut args = Args::parse();
 
     // TODO Check if args.file is empty
     // If it is, read from stdin
 
-    // TODO Check if all files exist before starting
-
     // TODO What if file is not ascii?
 
-    // Iterate over all files
-    // Concatenation will happen because files will be printed out in order
+    // TODO Change exit codes? Maybe copy from cat
+
+    // TODO Treat other args
+
+    // TODO Maybe unnecessary?
+    if args.file.is_empty() {
+        args.file.push("-".to_string());
+    }
+    else {  // Only check files if any have been input by user
+        if !check_files(&args.file) {
+            std::process::exit(1);
+        }
+    }
 
     let mut line_counter = 0;
     for file_name in &args.file {
-        // TODO Treat for -
 
-        let file = match File::open(file_name) {
-            Ok(f) => f,
-            Err(_) => {
-                eprintln!("Failed to open file");
-                std::process::exit(1);
+        if file_name == "-" {  // read from stdin
+            match print_file(&mut BufReader::new(stdin().lock()), &mut line_counter) {
+                Ok(()) => (),
+                Err(_) => {
+                    println!("Error printing stdin");
+                    std::process::exit(1);
+                }
             }
-        };
+        }
+        else {  // normal file
+            let file = match File::open(file_name) {
+                Ok(f) => f,
+                Err(_) => {
+                    eprintln!("Error reading file");
+                    std::process::exit(1);
+                }
+            };
 
-        match print_file(file, &mut line_counter) {
-            Ok(()) => (),
-            Err(_) => {
-                println!("Error printing file");
-                std::process::exit(1);
+            // TODO Fix this for raw data files
+            // Currently it fails if we try reading a file that is not UTF-8
+            match print_file(&mut BufReader::new(file), &mut line_counter) {
+                Ok(()) => (),
+                Err(_) => {
+                    println!("Error printing file");
+                    std::process::exit(1);
+                }
             }
         }
     }
